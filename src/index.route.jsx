2a88626ea584a51fe88e15dbf08e8886346e875a
vdkhoa7498 <vdkhoa7498@gmail.com>
2021-06-17 02:35:28 +0700

    Update router

--- src/index.route.jsx
+++ src/index.route.jsx
@@ -1,25 +1,58 @@
 import React, { Suspense } from 'react';
 import { Switch, Route, BrowserRouter as Router } from 'react-router-dom';
+import { Layout } from 'antd';
 
-// import Home from '../pages/home/Home';
-// import NotFound from '../pages/notFound/NotFound';
-import Register from './pages/register/Register';
-import Admin from './pages/admin/Admin';
+import HeaderBar from "./components/headerBar/HeaderBar";
+
+import NotFound from './pages/notFound/NotFound';
+import Register from './pages/auth/Register';
+import Login from './pages/auth/Login';
+import Home from './pages/home/Home';
+
+const { Header, Content, Footer } = Layout;
+
+function RouteLayout(props) {
+  const { children } = props;
+
+  return (
+    <Layout className="private-layout-container">
+      <Header className="header">
+        <HeaderBar />
+      </Header>
+      <Content style={{ padding: "0 24px", minHeight: 380 }}>
+        {children}
+      </Content>
+      <Footer style={{ textAlign: "center" }}>Website Learning @2021</Footer>
+    </Layout>
+  );
+}
 
 function RouterOutlet(props) {
   return (
     <Suspense fallback={null}>
       <Router>
         <Switch>
+          <Route
+            exact
+            path={["/"]}
+          >
+            <RouteLayout>
+              <Switch>
+              <Route path="/">
+                <Home/>
+              </Route>
+              </Switch>
+            </RouteLayout>
+          </Route>
           <Route path="/register">
               <Register/>
           </Route>
-          <Route path="/admin">
-            <Admin/>
+          <Route path="/login">
+              <Login/>
           </Route>
-          {/* <Route path="*">
+          <Route path="*">
             <NotFound/>
-          </Route> */}
+          </Route>
         </Switch>
       </Router>
     </Suspense>
