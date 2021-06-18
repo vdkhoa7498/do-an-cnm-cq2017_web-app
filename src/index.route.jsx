f560712f2cddcf387277e46ce4eb81696b275004
vdkhoa7498 <vdkhoa7498@gmail.com>
2021-06-19 01:12:17 +0700

    router

--- src/index.route.jsx
+++ src/index.route.jsx
@@ -7,6 +7,8 @@ import Login from './pages/auth/Login';
 import LayoutComponent from './components/layoutComponent/LayoutComponent';
 import Project from './pages/project/Project';
 import Transaction from './pages/transaction/Transaction';
+import ProjectItem from './pages/project/ProjectItem';
+import CreateProject from './pages/project/CreateProject';
 
 function RouterOutlet(props) {
   return (
@@ -18,10 +20,16 @@ function RouterOutlet(props) {
           </Route>
           <Route
             exact
-            path={["/projects", "/transactions"]}
+            path={["/projects", "/projects/:id", "/create-new-project","/transactions"]}
           >
             <LayoutComponent>
               <Switch>
+                <Route path="/create-new-project">
+                  <CreateProject/>
+                </Route>
+                <Route path="/projects/:id">
+                  <ProjectItem/>
+                </Route>
                 <Route path="/projects">
                   <Project/>
                 </Route>
