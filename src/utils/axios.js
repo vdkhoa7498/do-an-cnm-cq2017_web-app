b9b50fa7d3bf7a96601bc7a8a2a8da0305b89e9f
Dang Khoa <khoavo@sbl.dev>
2021-06-27 15:06:13 +0700

    update .env

--- src/utils/axios.js
+++ src/utils/axios.js
@@ -1,6 +1,8 @@
 import Axios from "axios";
+const server_address = process.env.REACT_APP_SERVER || `http://localhost:8000`
+console.log("server_address", process.env.REACT_APP_SERVER)
 let axios = Axios.create({
-  baseURL: `http://localhost:8000`,
+  baseURL: server_address,
   headers: { "Content-Type": "application/json" },
 });
 
