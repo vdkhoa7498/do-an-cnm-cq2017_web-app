c01eec760b43f6b7c5d6c6fb98607e2dffda59dc
Dang Khoa <khoavo@sbl.dev>
2021-06-27 19:12:43 +0700

    donate & resend transactions

--- src/pages/transaction/Donate.jsx
+++ src/pages/transaction/Donate.jsx
@@ -1,9 +1,85 @@
-import Transaction from "./Transaction"
+import { useEffect, useState } from "react";
+import "./styles.scss";
+import { Table, Divider, Input } from "antd";
+const { TextArea } = Input;
+const columns = [
+  {
+    title: "From address",
+    dataIndex: "donate_txs_from_address",
+    key: "donate_txs_from_address",
+    render: text => {return(<TextArea autoSize={{ minRows: 2, maxRows: 6 }} value={text} />)},
+  },
+  {
+    title: "To address",
+    dataIndex: "donate_txs_to_address",
+    key: "donate_txs_to_address",
+    render: (text) => {
+      return <TextArea autoSize={{ minRows: 2, maxRows: 6 }} value={text} />;
+    },
+  },
+  {
+    title: "Amount",
+    dataIndex: "donate_txs_amount",
+    key: "donate_txs_amount",
+    colSpan: 1,
+  },
+  {
+    title: "Date",
+    dataIndex: "donate_txs_timestamp",
+    key: "donate_txs_timestamp",
+    render: (donateDate) => {
+      const date_ob = new Date(donateDate);
+      console.log(date_ob);
+      const year = date_ob.getFullYear();
+      const month = ("0" + (date_ob.getMonth() + 1)).slice(-2);
+      const date = ("0" + date_ob.getDate()).slice(-2);
 
-const Donate = () =>{
-    return(
-        <Transaction title="User Donate List" />
-    )
-}
+      const hours = ("0" + date_ob.getHours()).slice(-2);
+      const minutes = ("0" + date_ob.getMinutes()).slice(-2);
+      const seconds = ("0" + date_ob.getSeconds()).slice(-2);
+      return (
+        <span>
+          {year}/{month}/{date} - {hours}:{minutes}:{seconds}
+        </span>
+      );
+    },
+    colSpan: 1,
+  },
+];
 
-export default Donate
+const sample = [
+  {
+    donate_txs_from_address:
+      "04eb0fbee3f7ea657d0343a33c812d7114d5d2ab7fabb1456fb3b4888382a18042f10038d5997e256ccd9592367af1da16fc683051aa02d2c4512148d4899353ff",
+    donate_txs_to_address:
+      "04eb0fbee3f7ea657d0343a33c812d7114d5d2ab7fabb1456fb3b4888382a18042f10038d5997e256ccd9592367af1da16fc683051aa02d2c4512148d4899353ff",
+    donate_txs_amount: "12000",
+    donate_txs_timestamp: "2021-06-27 00:16:34",
+  },
+];
+
+const Donate = () => {
+  const [donateList, setDonateList] = useState(sample);
+
+  useEffect(() => {}, []);
+
+  return (
+    <div
+      style={{
+        display: "flex",
+        alignItems: "flex-start",
+        flexDirection: "column",
+      }}
+    >
+      <h1>User Donate List</h1>
+      <Divider />
+      <Table
+        columns={columns}
+        dataSource={donateList}
+        style={{ width: "100%" }}
+      />
+    </div>
+  );
+};
+
+export default Donate;
--- src/pages/transaction/Resend.jsx
+++ src/pages/transaction/Resend.jsx
@@ -1,9 +1,83 @@
-import Transaction from "./Transaction"
+import { useEffect, useState } from "react";
+import "./styles.scss";
+import { Table, Divider, Input } from "antd";
+const { TextArea } = Input;
+const columns = [
+  {
+    title: "From address",
+    dataIndex: "sendback_txs_from_address",
+    key: "sendback_txs_from_address",
+    render: text => {return(<TextArea autoSize={{ minRows: 2, maxRows: 6 }} value={text} />)},
+  },
+  {
+    title: "To address",
+    dataIndex: "sendback_txs_to_address",
+    key: "sendback_txs_to_address",
+    render: (text) => {
+      return <TextArea autoSize={{ minRows: 2, maxRows: 6 }} value={text} />;
+    },
+  },
+  {
+    title: "Amount",
+    dataIndex: "sendback_txs_amount",
+    key: "sendback_txs_amount",
+  },
+  {
+    title: "Date",
+    dataIndex: "sendback_txs_timestamp",
+    key: "sendback_txs_timestamp",
+    render: (resendDate) => {
+      const date_ob = new Date(resendDate);
+      console.log(date_ob);
+      const year = date_ob.getFullYear();
+      const month = ("0" + (date_ob.getMonth() + 1)).slice(-2);
+      const date = ("0" + date_ob.getDate()).slice(-2);
 
-const Resend = () =>{
-    return(
-        <Transaction title="Organization Resend List" />
-    )
-}
+      const hours = ("0" + date_ob.getHours()).slice(-2);
+      const minutes = ("0" + date_ob.getMinutes()).slice(-2);
+      const seconds = ("0" + date_ob.getSeconds()).slice(-2);
+      return (
+        <span>
+          {year}/{month}/{date} - {hours}:{minutes}:{seconds}
+        </span>
+      );
+    },
+  },
+];
 
-export default Resend
+const sample = [
+  {
+    sendback_txs_from_address:
+      "04eb0fbee3f7ea657d0343a33c812d7114d5d2ab7fabb1456fb3b4888382a18042f10038d5997e256ccd9592367af1da16fc683051aa02d2c4512148d4899353ff",
+    sendback_txs_to_address:
+      "04eb0fbee3f7ea657d0343a33c812d7114d5d2ab7fabb1456fb3b4888382a18042f10038d5997e256ccd9592367af1da16fc683051aa02d2c4512148d4899353ff",
+    sendback_txs_amount: "12000",
+    sendback_txs_timestamp: "2021-06-27 00:16:34",
+  },
+];
+
+const Resend = () => {
+  const [resendList, setResendList] = useState(sample);
+
+  useEffect(() => {}, []);
+
+  return (
+    <div
+      style={{
+        display: "flex",
+        alignItems: "flex-start",
+        flexDirection: "column",
+      }}
+    >
+      <h1>User Resend List</h1>
+      <Divider />
+      <Table
+        columns={columns}
+        dataSource={resendList}
+        style={{ width: "100%" }}
+      />
+    </div>
+  );
+};
+
+export default Resend;
