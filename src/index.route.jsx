6a60f13bff0c373449cc42878b211b79dfce7353
Dang Khoa <khoavo@sbl.dev>
2021-06-27 17:38:56 +0700

    fix menu

--- src/index.route.jsx
+++ src/index.route.jsx
@@ -1,15 +1,23 @@
-import React, { Suspense } from 'react';
-import { Switch, Route, BrowserRouter as Router, Redirect } from 'react-router-dom';
+import React, { Suspense } from "react";
+import {
+  Switch,
+  Route,
+  BrowserRouter as Router,
+  Redirect,
+} from "react-router-dom";
 
-import NotFound from './pages/notFound/NotFound';
-import Register from './pages/auth/Register';
-import Login from './pages/auth/Login';
-import LayoutComponent from './components/layoutComponent/LayoutComponent';
-import Project from './pages/project/Project';
-import Transaction from './pages/transaction/Transaction';
+import NotFound from "./pages/notFound/NotFound";
+import Register from "./pages/auth/Register";
+import Login from "./pages/auth/Login";
+import LayoutComponent from "./components/layoutComponent/LayoutComponent";
+import Project from "./pages/project/Project";
+import Donate from "./pages/transaction/Donate";
+import ConfirmProjectList from "./pages/project/ConfirmProjectList"
+import UnconfirmProjectList from "./pages/project/UnconfirmProjectList"
 
-import ProjectItem from './pages/project/ProjectItem';
-import CreateProject from './pages/project/CreateProject';
+import ProjectItem from "./pages/project/ProjectItem";
+import CreateProject from "./pages/project/CreateProject";
+import Resend from "./pages/transaction/Resend";
 
 function RouterOutlet(props) {
   return (
@@ -17,42 +25,59 @@ function RouterOutlet(props) {
       <Router>
         <Switch>
           <Route exact path="/">
-            <Redirect to = "/projects"/>
+            <Redirect to="/projects" />
           </Route>
           <Route
             exact
-            path={["/projects", "/projects/:id", "/create-new-project","/transactions"]}
+            path={[
+              "/projects",
+              "/projects/:id",
+              "/create-new-project",
+              "/donate-transactions",
+              "/resend-transactions",
+              "/confirmed-projects",
+              "/unconfirmed-projects"
+            ]}
           >
             <LayoutComponent>
               <Switch>
                 <Route path="/create-new-project">
-                  <CreateProject/>
+                  <CreateProject />
                 </Route>
                 <Route path="/projects/:id">
-                  <ProjectItem/>
+                  <ProjectItem />
                 </Route>
                 <Route path="/projects">
-                  <Project/>
+                  <Project />
                 </Route>
-                <Route path="/transactions">
-                  <Transaction/>
+                <Route path="/confirmed-projects">
+                  <ConfirmProjectList />
+                </Route>
+                <Route path="/unconfirmed-projects">
+                  <UnconfirmProjectList />
+                </Route>
+                <Route path="/donate-transactions">
+                  <Donate />
+                </Route>
+                <Route path="/resend-transactions">
+                  <Resend />
                 </Route>
               </Switch>
             </LayoutComponent>
           </Route>
           <Route path="/register">
-              <Register/>
+            <Register />
           </Route>
           <Route path="/login">
-              <Login/>
+            <Login />
           </Route>
           <Route path="*">
-            <NotFound/>
+            <NotFound />
           </Route>
         </Switch>
       </Router>
     </Suspense>
-  )
+  );
 }
 
-export default RouterOutlet
+export default RouterOutlet;
